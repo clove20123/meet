8b0666b2a77c573ea6c6a03ef6ff200c18387d3b
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-09-06 13:09:25 +0200

    added static-site-test

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,8 +13,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://clove20123.github.io/meet/"],
-  javascript_origins: ["https://clove20123.github.io", "http://localhost:3000"],
+  redirect_uris: ["https://clove20123.github.io/meet"],
+  javascript_origins: ["https://clove20123.github.io", "http://localhost:8080"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -34,10 +34,53 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
       authUrl: authUrl,
     }),
   };
 };
+
+module.exports.getAccessToken = async (event) => {
+  // The values used to instantiate the OAuthClient are at the top of the file
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+  
+    return new Promise((resolve, reject) => {
+      /**
+       *  Exchange authorization code for access token with a “callback” after the exchange,
+       *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+       */
+  
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(token);
+      });
+    })
+    .then((token) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(token),
+      };
+    })
+      .catch((err) => {
+        // Handle error
+        console.error(err);
+        return {
+          statusCode: 500,
+          body: JSON.stringify(err),
+        };
+      });
+  };
