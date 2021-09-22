01508501fd75fa50278d5f45fd3e2863cf1c60b5
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-09-22 11:58:13 +0200

    added 2 features

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,8 +4,12 @@ describe('show/hide an event details', () => {
   let browser;
   let page;
   beforeAll(async () => {
-    jest.setTimeout(30000);
-    browser = await puppeteer.launch();
+    
+    browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 250, // slow down by 250ms
+     ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+    });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
@@ -18,17 +22,17 @@ describe('show/hide an event details', () => {
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
-  });
+  }, 20000);
 
   test('User can expand an event to see its details', async () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeDefined();
-  });
+  }, 20000);
 
   test('User can collapse an event to hide its details', async () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
-  });
+  }, 20000);
 });
