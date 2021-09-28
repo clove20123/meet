5fcab896c913be4d12862dcb866706f514b93a93
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-09-28 10:03:39 +0200

    added ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -18,14 +18,24 @@ state = {
 
   handleInputChanged = (event) => {
     const number = event.target.value;
-    if (number >= 1 && number <= 32) 
-    this.setState({ numberOfEvents: number })
+    if (number < 1 || number > 32) {
+    return this.setState({
+      errorText: 'Please enter a number between 1 and 32.',
+      numberOfEvents: ''
+    });
+  } else {
+    this.setState({ 
+      numberOfEvents: number,
+      errorText:'' 
+    });
     this.props.updateNumberOfEvents(number);
+  }
   };
 
   render () {
   return (
    <div className="NumberOfEvents">
+     <ErrorAlert text={this.state.errorText}/>
      <p>Limit search results</p>
      <input 
      type="number" 
@@ -35,6 +45,7 @@ state = {
      placeholder="1-32" 
      onChange={this.handleInputChanged} 
      />
+     
    </div>
   )}
 }
